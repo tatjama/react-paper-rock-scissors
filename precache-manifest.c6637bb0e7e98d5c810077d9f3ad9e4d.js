8c0c3e8a0c9511af92dd1e057adddd9f70a57a80
tatjama <tanja120a@gmail.com>
2020-04-22 21:10:14 +0200

    Updates

---
+++ precache-manifest.c6637bb0e7e98d5c810077d9f3ad9e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c05bef0b473ceb32019e0a5daa0341e",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/index.html"
+  },
+  {
+    "revision": "24c5d44babe6e326be11",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d018ea7ed403d8cbf087",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/js/2.0ae93867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/js/2.0ae93867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c5d44babe6e326be11",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/js/main.7d351f56.chunk.js"
+  },
+  {
+    "revision": "1d034d44459efd671392",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/js/runtime-main.8537a9ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/https/tatjama/github.io/react-paper-rock-scissors/static/media/logo.5d5d9eef.svg"
+  }
+]);
