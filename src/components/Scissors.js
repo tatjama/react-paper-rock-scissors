56dae6f5ae9e1c3c015def35bc848cef61dbb888
tatjama <tanja120a@gmail.com>
2020-04-23 13:43:45 +0200

    Inline css in components icon

--- src/components/Scissors.js
+++ src/components/Scissors.js
@@ -3,8 +3,12 @@ import scissors from '../images/icon-scissors.svg';
 
 class Scissors extends React.Component{
     render(){
+        const myStyle  = {        
+            backgroundColor:  "hsl(39, 89%, 49%)",
+            boxShadow: "0px 10px hsla(40, 84%, 53%, 0.7)"
+        }
         return(
-            <div onclick="pickCardPlayer(this, 5, pickCardHouse, playAgain, '.second')"  
+            <div style={myStyle} onclick="pickCardPlayer(this, 5, pickCardHouse, playAgain, '.second')"  
                     className="icon scissors">
                         <div className="icon-bg">
                             <img src={scissors} alt="scissors"/>
